b7b56e97ea9db3f4b5a1bb7fdce61bac7df40f29
Yasssssh <yk33695@gmail.com>
2023-07-13 23:16:53 +0530

    Added content in aboutejs

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,72 +1,72 @@
-const express = require ("express");
+const express = require("express");
 const router = express.Router();
 const Post = require("../models/post");
 
 //Routes Get HOME
-router.get("/",async (req, res)=>{
-    try{
-        const locals = {
-            title: "Nodejs Blog",
-            description : "Simple blog created with nodejs"
-        }
-        let perPage = 10;
-        let page = req.query.page || 1;
+router.get("/", async (req, res) => {
+  try {
+    const locals = {
+      title: "Nodejs Blog",
+      description: "Simple blog created with nodejs",
+    };
+    let perPage = 10;
+    let page = req.query.page || 1;
 
-        const data = await Post.aggregate([{$sort:{createdAt: -1}}])
-        .skip(perPage * page - perPage)
-        .limit(perPage)
-        .exec();
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
 
-        const count = await Post.count();
-        const nextPage = parseInt(page) + 1;
-        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+    const count = await Post.count();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
 
-
-        res.render('index',{locals, data, current: page, nextPage : hasNextPage ? nextPage : null, currentRoute: '/'});
-    }catch(err){
-        console.log(err);
-    }
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //Routes Get specificpost
-router.get("/post/:id",async (req, res)=>{
-    try{
-        let slug = req.params.id;
-        const data = await Post.findById({_id: slug});
-        const locals = {
-            title: data.title,
-            description: "Simple blog created with nodejs, express and mongodb"
-        };
-        res.render('post',{locals, data, currentRoute: `/post/${slug}`});
-    }
-    catch(err){
-        console.log(err);
-    }
+router.get("/post/:id", async (req, res) => {
+  try {
+    let slug = req.params.id;
+    const data = await Post.findById({ _id: slug });
+    const locals = {
+      title: data.title,
+      description: "Simple blog created with nodejs, express and mongodb",
+    };
+    res.render("post", { locals, data, currentRoute: `/post/${slug}` });
+  } catch (err) {
+    console.log(err);
+  }
 });
- 
+
 //Routes post searchpost
-router.post("/search",async (req, res)=>{
-    try{
-        const locals = {
-            title: "Search",
-            description: "Simple blog created with nodejs, express and mongodb"
-        };
-        let searchTerm = req.body.searchTerm;
-        let searchnoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9]/g,"");
-        const data = await Post.find({
-            $or:[
-                {title : {$regex: new RegExp(searchnoSpecialChar, 'i')}},
-                {body : {$regex: new RegExp(searchnoSpecialChar, 'i')}}
-            ]
-        });
-        res.render('search',{locals, data});
-    }
-    catch(err){
-        console.log(err);
-    }
+router.post("/search", async (req, res) => {
+  try {
+    const locals = {
+      title: "Search",
+      description: "Simple blog created with nodejs, express and mongodb",
+    };
+    let searchTerm = req.body.searchTerm;
+    let searchnoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9]/g, "");
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchnoSpecialChar, "i") } },
+        { body: { $regex: new RegExp(searchnoSpecialChar, "i") } },
+      ],
+    });
+    res.render("search", { locals, data });
+  } catch (err) {
+    console.log(err);
+  }
 });
-
-
 
 // function insertPostData(){
 //     Post.insertMany([
@@ -118,8 +118,8 @@ router.post("/search",async (req, res)=>{
 // }
 // insertPostData();
 
-router.get("/about",(req, res)=>{
-    res.render("about", {currentRoute: '/about'});
+router.get("/about", (req, res) => {
+  res.render("about");
 });
 
 module.exports = router;
