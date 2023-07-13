cee74e6a78a3a0e21ccffb32b948e821589cd62f
Yasssssh <yk33695@gmail.com>
2023-07-13 22:13:52 +0530

    disabled register route fr this time

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -39,45 +39,45 @@ router.get("/admin", async (req, res) => {
 });
 
 //Routes post register
-router.post("/register", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const hashedpassword = await bcrypt.hash(password, 10);
-    try {
-      const user = await User.create({ username, password: hashedpassword });
-      res.status(200).json({ message: "User Created", user });
-    } catch (error) {
-      if (error.code === 11000) {
-        res.status(409).json({ message: "User already registered" });
-      }
-      res.status(500).json({ message: "Internal Server Error" });
-    }
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-//Routes post login
-// router.post("/admin", async (req, res) => {
+// router.post("/register", async (req, res) => {
 //   try {
 //     const { username, password } = req.body;
-//     const user = await User.findOne({ username });
-//     if (!user) {
-//       return res.status(401).json({ message: "invalid credentials" });
-//     } else {
-//       const isPasswordValid = await bcrypt.compare(password, user.password);
-//       if (!isPasswordValid) {
-//         return res.status(401).json({ message: "invalid credentials" });
+//     const hashedpassword = await bcrypt.hash(password, 10);
+//     try {
+//       const user = await User.create({ username, password: hashedpassword });
+//       res.status(200).json({ message: "User Created", user });
+//     } catch (error) {
+//       if (error.code === 11000) {
+//         res.status(409).json({ message: "User already registered" });
 //       }
-//       const token = jwt.sign({ userId: user._id }, jwtSecret);
-//       res.cookie("token", token, { httpOnly: true });
-
-//       res.redirect("/dashboard");
+//       res.status(500).json({ message: "Internal Server Error" });
 //     }
 //   } catch (err) {
 //     console.log(err);
 //   }
 // });
+
+//Routes post login
+router.post("/admin", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: "invalid credentials" });
+    } else {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "invalid credentials" });
+      }
+      const token = jwt.sign({ userId: user._id }, jwtSecret);
+      res.cookie("token", token, { httpOnly: true });
+
+      res.redirect("/dashboard");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 //Routes get Admin dashboard
 router.get("/dashboard", authMiddleWare, async (req, res) => {
