3d2e586e577f9f6d5d8b631f880a2f2bbd0a6565
Yash Kamble <108587335+Yasssssh@users.noreply.github.com>
2023-07-13 20:55:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const session = require("express-session");
 const {isActiveRoute} = require("./server/helpers/routeHelpers");
 
 const app = express();
-const PORT = 3000 || process.env.PORT;
+const PORT = 3000;
 
 //connect to db
 connectDB();
