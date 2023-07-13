7ab3cd18601e0f968a2a505ad303311eb6f9ba59
Yasssssh <yk33695@gmail.com>
2023-07-13 21:33:06 +0530

    db.js update

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,4 @@
 const mongoose = require("mongoose");
-const connectDB = async () => {
-  try {
-    mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(
-      "mongodb+srv://MAGi:pNeDMUqjnRWoVoNH@nodejs-blogdb.igbucbc.mongodb.net/"
-    );
-    console.log(`Database Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const mongoose = require("mongoose");
 
 const connectDataBase = async () => {
   try {
