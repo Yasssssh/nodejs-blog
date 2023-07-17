bdc9d763d08265c7c40ea6efc69897429b687aed
Yasssssh <yk33695@gmail.com>
2023-07-17 11:53:22 +0530

    added image tag to post

--- server/models/post.js
+++ server/models/post.js
@@ -1,22 +1,26 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    body: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now
-    }
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  image: {
+    data: Buffer,
+    contentType: String,
+  },
 });
 
-module.exports = mongoose.model('Post', PostSchema);
+module.exports = mongoose.model("Post", PostSchema);
